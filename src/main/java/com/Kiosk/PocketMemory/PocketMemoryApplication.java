a544fa4a9370e813846e92c79a416d456c208ad8
truthgarnet <57488270+truthgarnet@users.noreply.github.com>
2021-09-24 10:35:54 +0900

    Initial commit

---
+++ src/main/java/com/Kiosk/PocketMemory/PocketMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.Kiosk.PocketMemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketMemoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Kiosk/PocketMemory/PocketMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kiosk.PocketMemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketMemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
